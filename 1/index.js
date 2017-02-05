d8c96a8ee659619cb3b980b32ef7583719fca1b8
Yan Lobaty <yanlavryushev@gmail.com>
2017-02-05 13:52:25 +0400

    redo

--- 1/index.js
+++ 1/index.js
@@ -1,12 +1,9 @@
-function calculate(a, b) {
-	let aa = ~~a.split('').reduceRight((previousValue, currentValue, index, array) => {
-	  	return ~~previousValue + Math.pow(2, array.length - index - 1)*currentValue;
-	});
-	let bb = ~~b.split('').reduceRight((previousValue, currentValue, index, array) => {
-	  return ~~previousValue + Math.pow(2, array.length - index - 1)*currentValue;
-	});
-	console.log(aa+bb);
-	return aa+bb;
+function calculate() {
+	return Object.values(arguments).reduce((previousValue, currentValue, index, array) => {
+		return previousValue + currentValue.split('').reverse().reduce(function(x, y, i){
+	      return (y === '1') ? x + Math.pow(2, i) : x;
+	    },0);
+	},0);;
 }
 
 console.assert(calculate('10', '10') === 4);
